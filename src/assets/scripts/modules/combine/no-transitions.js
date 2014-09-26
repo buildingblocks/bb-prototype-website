593ed1228e4175d224090e7d7c4604f105609036
Paul Welsh <p.welsh@building-blocks.com>
2014-09-26 10:10:12 +0100

    removed setGlobal from js

--- src/assets/scripts/modules/combine/no-transitions.js
+++ src/assets/scripts/modules/combine/no-transitions.js
@@ -4,10 +4,6 @@ var bb = bb ? bb : {};
 		toggleTransitions: {
 			bb: bb,
 			noTransitionsClass: 'no-transitions',
-			setGlobal: function (bb) {
-				var self = this;
-				self.bb = bb;
-			},
 			disableTransitions: function () {
 				var self = this;
 				self.bb.settings.$html.addClass(self.noTransitionsClass);
@@ -17,9 +13,6 @@ var bb = bb ? bb : {};
 				self.bb.settings.$html.removeClass(self.noTransitionsClass);
 			}
 		}
-	});
-	$.subscribe('setGlobal', function (event, bb) {
-		bb.toggleTransitions.setGlobal(bb);
 	});
 	$.subscribe('pageReady', function () {
 		bb.toggleTransitions.disableTransitions();
