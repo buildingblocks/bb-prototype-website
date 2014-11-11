5d2cb84f708e7337afa8c9ecbaf52a30bf798f1e
Daniel Furze <daniel@furzeface.com>
2014-11-11 10:47:51 +0000

    fixed menu module using old setGlobal parts

--- src/assets/scripts/modules/combine/menu.js
+++ src/assets/scripts/modules/combine/menu.js
@@ -1,37 +1,28 @@
-/**
- * @file Menu
- * @version 0.6.2
- * @author {@link https://github.com/buildingblocks Building Blocks}
- */
 var bb = bb ? bb : {};
 (function ($) {
 	$.extend(bb, {
-		/**
-        * Menu related methods.
-        * @namespace menu
-        */
 		menu: {
 			$handle : null,
 			menuInClass : 'menu-in',
-			init : function () {
+			init: function () {
 				var self = this;
 				self.$handle = $('.action-menu');
 				self.$handle.on('click.menu', function (event) {
 					event.preventDefault();
-					if (self.bb.settings.$html.hasClass(self.menuInClass)) {
+					if (bb.settings.$html.hasClass(self.menuInClass)) {
 						self.closeMenu(event);
 					} else {
 						self.openMenu(event);
 					}
 				});
 			},
-			openMenu : function () {
+			openMenu: function () {
 				var self = this;
-				self.bb.settings.$html.addClass(self.menuInClass);
+				bb.settings.$html.addClass(self.menuInClass);
 			},
-			closeMenu : function () {
+			closeMenu: function () {
 				var self = this;
-				self.bb.settings.$html.removeClass(self.menuInClass);
+				bb.settings.$html.removeClass(self.menuInClass);
 			}
 		}
 	});
